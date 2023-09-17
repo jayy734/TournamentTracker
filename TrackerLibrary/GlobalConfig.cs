d0a0e822c1051ec9d72bdf8fb5f282e58bf05d81
jayy734 <117484060+jayy734@users.noreply.github.com>
2023-09-17 07:49:33 +0300

    Connected SQL using dapper.

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
+using System.Configuration;
+using System.Runtime.InteropServices;
+
+namespace TrackerLibrary
+{
+    public class GlobalConfig
+    {
+        public static IDataConnection Connection { get; private set; } 
+        public static void InitializeConnections(DatabaseType db)
+        {
+            switch (db)
+            {
+                case DatabaseType.sql:
+                    break;
+                case DatabaseType.TextFile:
+                    break;
+                default: 
+                    break;
+            }
+            if (db == DatabaseType.sql)
+            {   
+                //TODO - set up the sql connector properly 
+                SqlConnector sql = new SqlConnector();
+                Connection = sql;
+            }
+            else if (db == DatabaseType.TextFile) 
+            {
+                // TODO - Create the text connection
+                TextConnector text = new TextConnector();
+                Connection = text;
+            }
+
+        }
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
